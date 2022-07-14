ddee2f93a2dff63bca0363cc0ef873df7f949c68
ravifrancesco <ravi.srinivasan99@gmail.com>
2022-07-14 06:18:04 -0400

    Added doc

--- pepita/models/layers/ConsistentDropout.py
+++ pepita/models/layers/ConsistentDropout.py
@@ -2,7 +2,13 @@ import torch
 import torch.nn as nn
 
 class ConsistentDropout(nn.Module):
+    r"""Dropout Layer that requires manual reset of the mask
+    """
     def __init__(self, p=0.5):
+        r"""
+        Args:
+            p (float, optional): dropout probability (default is 0.5)
+        """
         super().__init__()
 
         if p < 0 or p > 1:
@@ -15,6 +21,8 @@ class ConsistentDropout(nn.Module):
         return "p={}".format(self.p)
 
     def reset_mask(self):
+        r"""Reset dropout mask
+        """
         self.mask = None
 
     def train(self, mode=True):
@@ -23,9 +31,25 @@ class ConsistentDropout(nn.Module):
             self.reset_mask()
 
     def _get_sample_mask_shape(self, sample_shape):
+        r"""Returns mask shape
+
+        Args:
+            sample_shape (shape): sample shape
+
+        Returns:
+            sample shape (shape)
+        """
         return sample_shape
 
     def _create_mask(self, input):
+        r"""Creates the mask by drawing from Bernoulli distribution with probability p
+
+        Args:
+            input (torch.Tensor): input
+        
+        Returns:
+            mask (torch.Tensor): dropout mask
+        """
         mask_shape = input.shape
         mask = torch.empty(mask_shape, dtype=torch.bool).bernoulli_(self.p)
         return mask
